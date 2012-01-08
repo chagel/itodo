7a586f30fe13d69978c2ac868bfcbbf95bcfef3a
Mike Chen <chagel@gmail.com>
2012-01-08 19:29:42 +0800

    init project

---
+++ itodo.py
@@ -0,0 +1,24 @@
+import sublime, sublime_plugin
+from datetime import datetime 
+
+class ItodoCommand(sublime_plugin.TextCommand):
+  def run(self, edit):
+    # only work on .todo files
+    filename = self.view.file_name()
+    if filename is None or not filename.endswith('.todo'):
+      return False
+    
+    for region in self.view.sel():
+      line = self.view.line(region)
+      line_contents = self.view.substr(line).strip()
+
+      # prepend @done if item is ongoing
+      if line_contents.startswith('-'):
+        self.view.insert(edit, line.end(), " @done (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
+        self.view.replace(edit, self.view.word(line.begin()), "+ ")
+      # undo @todo
+      elif line_contents.startswith('+'):
+        subfix = self.view.find('(\s)*@done(.)+\)$', line.begin())
+        self.view.erase(edit, subfix)
+        self.view.replace(edit, self.view.word(line.begin()), "- ")
+      
---
+++ readme.md
@@ -0,0 +1,40 @@
+iTodo for Sublime Text
+------------------
+
+iTodo is a GTD tool for managing todo list in Sublime Text editor.
+
+Description
+------------------
+
+This plugin contains one text command names 'itodo' and a syntax definition with the same name.
+
+Usage 
+------------------
+
+"CMD + \": toggle task completed
+
+
+Samples 
+------------------
+
+Suppose we have a following todo file:
+
+	Project A:
+		- call mum tomorrow at 8 am.
+
+Highlight this item line and press "CMD + \", it marks a tag "@done" and also appends timestamp.
+
+	Project A:
+		+ call mum tomorrow at 8 am. @done (2012-01-08 18:12)
+
+
+Contribution
+------------------
+
+Thanks Taskmate for TextMate (https://github.com/svenfuchs/taskmate).
+
+
+Source / Installation 
+------------------
+
+https://github.com/chagel/itodo
