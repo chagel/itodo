92ae0313b5398af63530f54a8637c97f0082f67f
Mike Chen <chagel@gmail.com>
2012-09-05 00:05:18 +0800

    add ClearCommand to clean completed tasks

--- itodo.py
+++ itodo.py
@@ -2,11 +2,11 @@ import sublime, sublime_plugin
 from datetime import datetime 
 
 class ItodoBase(sublime_plugin.TextCommand):
+  COMPLETED_MARK = "@done"
+
   def run(self, edit):
     filename = self.view.file_name()
     # list of allowed filetypes
-    # (added this because some people, like me, tend to use .txt files because they're more universal)
-    # (this means that i have to manually set syntax for such file, but it's no sweat ;)
     allowed_filetypes = ('.todo', '.txt')
     if filename is None or not filename.endswith(allowed_filetypes):
       return False  
@@ -33,11 +33,18 @@ class CompleteCommand(ItodoBase):
       line_contents = self.view.substr(line).strip()
       # prepend @done if item is ongoing
       if line_contents.startswith('-'):
-        self.view.insert(edit, line.end(), " @done (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
+        self.view.insert(edit, line.end(), " "+ ItodoBase.COMPLETED_MARK +" (%s)" % datetime.now().strftime("%Y-%m-%d %H:%M"))
         self.view.replace(edit, line_head, "+")
       # undo @todo
       elif line_contents.startswith('+'):
-        subfix = self.view.find('(\s)*@done(.)+\)$', line.begin())
+        subfix = self.view.find('(\s)*'+ ItodoBase.COMPLETED_MARK +'(.)+\)$', line.begin())
         self.view.erase(edit, subfix)
         self.view.replace(edit, line_head, "-")
+      
+class ClearCommand(ItodoBase):
+  def runCommand(self, edit):    
+    if not sublime.ok_cancel_dialog("Are you sure to clear all completed items?"):
+      return False
+    for region in self.view.find_all(ItodoBase.COMPLETED_MARK):
+      self.view.erase(edit, self.view.full_line(region))
       
